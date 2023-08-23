4898d327a5a9bc01ed5358f0339857a56b5ed474
mamunsj <mamunsj2616@gmail.com>
2023-08-23 17:33:07 +0600

    my first react code

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("How was your day Ujjal");
+
+ReactDOM.render(
+  <h1>Hello folks, This is Ujjal writing code for development</h1>,
+  document.getElementById("root")
+);
+
+ReactDOM.render(
+  <ol>
+    <li>Ujjal</li>
+    <li>Tareq</li>
+  </ol>,
+  document.getElementById("root2")
+);
+
+ReactDOM.render(
+  <div>
+    <h1 className="header">This is another way of writing react code</h1>
+    <p>Let's add a 'P' element</p>
+  </div>,
+  document.getElementById("root")
+);
