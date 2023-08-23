360534692d880d23f40f394ebeb8be5bc1f1cce5
mamunsj <mamunsj2616@gmail.com>
2023-08-23 19:37:46 +0600

    declarative custom component

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-ReactDOM.render(
-  <div>
-    <h1 className="header">This is another way of writing react code</h1>
-    <p>Let's add a 'P' element</p>
-  </div>,
-  document.getElementById("root")
-);
+// Customs component
 
-const page = (
-  <div>
-    <h2 class="header">This is JSX</h2>
-    <p>This is a paragraph</p>
-  </div>
-);
+function Page() {
+  return (
+    <ol>
+      <li>It is mostly used</li>
+      <li>It is mostly used</li>
+      <li>It is mostly used</li>
+      <li>It is mostly used</li>
+      <li>It is mostly used</li>
+    </ol>
+  );
+}
 
-ReactDOM.render(page, document.getElementById("root"));
-
-console.log(page);
-
-const navbar = (
-  <nav className="nav">
-    <h1>Bandhan</h1>
-    <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  </nav>
-);
-
-ReactDOM.render(navbar, document.getElementById("root"));
+// ReactDOM.render(Page(), document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
+console.log(444);
