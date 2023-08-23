8b1c567424ce890b5eba3762c5831585de9db5c6
mamunsj <mamunsj2616@gmail.com>
2023-08-23 17:47:45 +0600

    challenge of adding nav element done

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-console.log("How was your day Ujjal");
-
-ReactDOM.render(
-  <h1>Hello folks, This is Ujjal writing code for development</h1>,
-  document.getElementById("root")
-);
-
-ReactDOM.render(
-  <ol>
-    <li>Ujjal</li>
-    <li>Tareq</li>
-  </ol>,
-  document.getElementById("root2")
-);
-
 ReactDOM.render(
   <div>
     <h1 className="header">This is another way of writing react code</h1>
@@ -20,3 +5,27 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
+
+const page = (
+  <div>
+    <h2 class="header">This is JSX</h2>
+    <p>This is a paragraph</p>
+  </div>
+);
+
+ReactDOM.render(page, document.getElementById("root"));
+
+console.log(page);
+
+const navbar = (
+  <nav className="nav">
+    <h1>Bandhan</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(navbar, document.getElementById("root"));
