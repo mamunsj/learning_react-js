a8551c7ea947c56ff982af68b05b0614ab447a0b
mamunsj <mamunsj2616@gmail.com>
2023-08-30 14:55:35 +0600

    added a custom hook

--- first-react-app/src/Home.js
+++ first-react-app/src/Home.js
@@ -1,47 +1,34 @@
 import { useState, useEffect } from "react";
 import BlogsList from "./Blogs";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  const handleDelete = (id) => {
-    const newBlog = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlog);
-    console.log(newBlog);
-  };
-
-  // useEffect only run after initial render and in every re-render means when the state is changed; we use dependencies separated by comma to explicity says what to do;
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
+  const [updatedBlogs, setUpdatedBlogs] = useState([]);
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch(" http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("Could not find the data");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setIsPending(false);
-          setBlogs(data);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
+    if (blogs) {
+      setUpdatedBlogs(blogs);
+    }
+  }, [blogs]);
+
+  const handleDelete = (id) => {
+    const newBlogs = updatedBlogs.filter((blog) => blog.id !== id);
+    setUpdatedBlogs(newBlogs);
+    console.log(newBlogs);
+  };
 
   return (
     <div className="home">
       {error && <div> {error} </div>}
       {isPending && <div>Loading...</div>}
-      {blogs && (
+      {updatedBlogs && (
         <BlogsList
-          blogs={blogs}
+          blogs={updatedBlogs}
           title="All the Blogs Post"
           handleDelete={handleDelete}
         />
